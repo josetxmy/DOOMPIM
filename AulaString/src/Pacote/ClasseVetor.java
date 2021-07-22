d1eb1bf7ac7baa00f098859642f17fc778f671e1
admin <admin@nbmpc058>
2021-07-22 14:29:24 -0300

    meu primeiro envio

---
+++ AulaString/src/Pacote/ClasseVetor.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pacote;
+
+/**
+ *
+ * @author admin
+ */
+public class ClasseVetor {
+    public double[] nome = new double [10];
+
+public void numeros(){
+    for(int v=9;v>=1;v--){
+        System.out.println(this.nome[v]);
+    }
+}
+}
