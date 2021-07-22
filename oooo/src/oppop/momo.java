89dbce396629dfd28b49b74df001192713954799
admin <admin@nbmpc058>
2021-07-22 14:23:39 -0300

    meu primeiro envio

---
+++ oooo/src/oppop/momo.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oppop;
+
+/**
+ *
+ * @author admin
+ */
+public class momo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     double m1,m2,soma,mg;  
+        m1=(8+9+7)/3;
+        m2=(4+6+6)/3;
+        soma=m1+m2;
+        
+        mg=soma/2;
+        System.out.println("A media dos valores 8,9 e 7 e:"+m1);
+        System.out.println("A media dos valores 4,5 e 6 e:"+m2);
+        System.out.println("A media geral e :"+mg);
+        
+    }
+    
+}
